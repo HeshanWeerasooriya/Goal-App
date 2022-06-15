d31f6846582eba121ad45a7667556ddb0efa446c
Heshan <heshan.imedha@gmail.com>
2022-06-15 22:00:35 +0530

    app ui layout

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your Course Goals !" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your Course Goals!" />
         <Button title="Add Goals" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of Goals...</Text>
       </View>
     </View>
@@ -16,6 +16,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex:1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   }
 });
